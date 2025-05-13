90191c1645c97838ac820327aea2fe2c28822d2d
pusilvija <silvijapupsaite@gmail.com>
2025-05-13 19:31:33 +0300

    added app link

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -8,6 +8,7 @@ function Projects() {
         <h3>Your-Planner App</h3>
         <p>CRUD app with authentication and REST API.</p>
         <div className="links">
+          <a href="https://your-planner-fe-production.up.railway.app/" target="_blank" rel="noopener noreferrer">Visit App</a>
           <a href="https://github.com/pusilvija/your-planner-fe" target="_blank" rel="noopener noreferrer">Frontend GitHub</a>
           <a href="https://github.com/pusilvija/your-planner" target="_blank" rel="noopener noreferrer">Backend GitHub</a>
         </div>
